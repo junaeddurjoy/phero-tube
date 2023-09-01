b0fb8c4078679255160de46c1d3aded74f7c772b
junaeddurjoy <junaed.durjoy@gmail.com>
2023-09-01 22:16:23 +0600

    fourth commit

--- js/load.js
+++ js/load.js
@@ -1,12 +1,12 @@
+// window.location.reload();
 const loadPhero = async (cat_id) => {
-    console.log(cat_id);
-    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/1001`);
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${cat_id}`);
     const data = await res.json();
     const pheroes = data.data;
     // console.log(pheroes)
     displayPheroes(pheroes);
 }
-loadPhero();
+// loadPhero();
 const displayPheroes = pheroes =>{
     const pheroContainer = document.getElementById('phero-container');
     pheroContainer.textContent = ' ';
@@ -21,22 +21,29 @@ const displayPheroes = pheroes =>{
                 pheroContainer.appendChild(pheroCard);
     }
     else{
-        pheroes.forEach(phero =>{
+        pheroContainer.classList = 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 pl-12 md:pl-7 lg:pl-7';
+        pheroes?.forEach(phero =>{
             // console.log(phero);
 
             const pheroCard = document.createElement('div');
             pheroCard.classList = 'card w-80 p-4 shadow-xl my-5';
 
-            if(phero.authors[0].verified == true){
+            if(phero?.authors[0]?.verified == true){
                 pheroCard.innerHTML = `
                 <figure class="relative"><img src="${phero.thumbnail}" alt="" />
-                <h3 class="absolute text-white rounded-xl bg-black bg-opacity-80 bottom-5 right-5">${phero.others.posted_date}</h3>
+                <h3 class="absolute text-white rounded-md bg-black bg-opacity-80 bottom-5 right-5">
+                ${handleDate(phero.others.posted_date)}
+                </h3>
                 </figure>
                 <div class="pt-5">
                     <div id="verified_logo" class="flex gap-2">
                         <img class="rounded-full h-8 w-8" src="${phero.authors[0].profile_picture}">
-                        <h2 class="card-title">${phero.title}</h2>
-                        <i class="fa-solid fa-star text-blue-400 pt-2"></i>
+                        <h2 class="card-title">${phero.title}
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0,0,256,256">
+                        <g fill="#5c7cfa" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(10.66667,10.66667)"><path d="M12,2c-5.5,0 -10,4.5 -10,10c0,5.5 4.5,10 10,10c5.5,0 10,-4.5 10,-10c0,-5.5 -4.5,-10 -10,-10zM17.7,9.7l-7,7c-0.2,0.2 -0.4,0.3 -0.7,0.3c-0.3,0 -0.5,-0.1 -0.7,-0.3l-3,-3c-0.4,-0.4 -0.4,-1 0,-1.4c0.4,-0.4 1,-0.4 1.4,0l2.3,2.3l6.3,-6.3c0.4,-0.4 1,-0.4 1.4,0c0.4,0.4 0.4,1 0,1.4z"></path></g></g>
+                        </svg>
+                        </h2>
+                        
                     </div>
                     <p class="pl-10">${phero.authors[0].profile_name}</p>
                     <p class="pl-10">${phero.others.views} views</p>
@@ -44,9 +51,12 @@ const displayPheroes = pheroes =>{
                 pheroContainer.appendChild(pheroCard);
             }
             else{
+
                 pheroCard.innerHTML = `
                 <figure class="relative"><img src="${phero.thumbnail}" alt="" />
-                <h3 class="absolute text-white rounded-xl bg-black bg-opacity-80 bottom-5 right-5">${phero.others.posted_date}</h3>
+                <h3 class="absolute text-white rounded-md bg-black bg-opacity-80 bottom-5 right-5">
+                ${handleDate(phero.others.posted_date)}
+                </h3>
                 </figure>
                 <div class="pt-5">
                     <div id="verified_logo" class="flex gap-2">
@@ -63,6 +73,19 @@ const displayPheroes = pheroes =>{
         })
     }
 }
+function handleDate(post_date){
+    console.log(post_date);
+    if(post_date != ''){
+        const postDate = parseFloat(post_date);
+        const hours = Math.floor(postDate / 3600);
+        const minutes = Math.floor((postDate % 3600) / 60);
+        const seconds = postDate % 60;
+        return(hours+'h'+minutes+'m'+seconds+'s ago')
+    }
+    else{
+        return('')
+    }
+}
 const loadCategory = async () => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
     const data = await res.json();
@@ -77,7 +100,7 @@ const displayCategories = categories => {
         const menuContainer = document.getElementById('menu-bar');
         const menuItem = document.createElement('div');
         menuItem.innerHTML = `
-            <button  id="menu-button" class="btn btn-secondary w-36">${categories[i].category}</button>
+            <button onclick="loadPhero(${categories[i].category_id})" id="menu-button" class="btn btn-secondary w-36">${categories[i].category}</button>
             `;
         menuContainer.appendChild(menuItem);
     }
